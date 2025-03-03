0af1523095634b022bc21b677d5208753d38eeb8
sinitskiy <anton.sinitskiy@gmail.com>
2025-03-03 14:08:02 -0500

    added more args to the main function to give more control from CLI

--- run_all.py
+++ run_all.py
@@ -16,9 +16,9 @@ The script can be run from command line with arguments:
 """
 
 # run this when you change utils.py to reload functions from utils.py
-import importlib
-import utils
-importlib.reload(utils)
+# import importlib
+# import utils
+# importlib.reload(utils)
 
 from utils import load_data, run_all, convert_to_array, draw_pairs_of_molecules
 import pandas as pd
@@ -30,6 +30,9 @@ if __name__ == '__main__':
         parser.add_argument('--encoding', type=str, default='chemformer', help='ML Encoding used for converting SMILES to vectors.')
         parser.add_argument('--upper_limit_ref_size', type=int, default=1000, help='Include reference species with number of molecules less than this value.')
         parser.add_argument('--lower_limit_ref_size', type=int, default=180, help='Include reference species with number of molecules greater or equal than this value.')
+        parser.add_argument('--size_threshold', type=int, default=20, help='Minimum number of molecules for a current species.')
+        parser.add_argument('--min_size_threshold', type=int, default=20, help='Minimum number of molecules for a current species to be included in slow low-level computations.')
+        parser.add_argument('--visualize_typical_pairs', type=bool, default=False, help='Visualize typical pairs of molecules.')
         args = parser.parse_args()
 
         data_folder = args.data_folder
@@ -77,8 +80,8 @@ if __name__ == '__main__':
                                 encoding_columns=encoding_columns,
                                 ref_chain=taxonomic_chain_ref.split('-'),
                                 max_taxonomic_distance=3,   # 2 for debugging, 3 for production runs
-                                size_threshold=20,
-                                min_size_threshold=20,
+                                size_threshold=args.size_threshold,
+                                min_size_threshold=args.min_size_threshold,
                                 distance_metric='Euclidean',
                                 percentiles=[25, 50],
                                 verbose=True,
@@ -92,29 +95,30 @@ if __name__ == '__main__':
                         pass
 
 
-        for taxonomic_chain_ref in taxonomic_chains_ref:
-                print(f"Starting with reference taxonomic chain: {taxonomic_chain_ref}")
-                
-                tdistance1=2
-                tdistance2=3
-                percentile=50
-                n_typical_pairs=3
-                
-                df_chemical_distances_vs_taxonomic_distances = pd.read_csv(f'{data_folder}/chem_vs_tax_dist_csv_files/chem_vs_tax_dist_{taxonomic_chain_ref}.csv')
-                if len(df_chemical_distances_vs_taxonomic_distances) == 0:
-                        print(f"No data for {taxonomic_chain_ref}")
-                        continue
-                for td in [tdistance1, tdistance2]:
-                        print(f"Tax dist {td}")
-                        dfc = df_chemical_distances_vs_taxonomic_distances[df_chemical_distances_vs_taxonomic_distances['taxonomic_distance'] == td].copy()
-                        distance_percentile_ave = dfc[f'distance_percentile_{percentile}'].mean()
-                        dfc['abs_difference_from_average_distance'] = (dfc[f'distance_percentile_{percentile}'] - distance_percentile_ave).abs()
-                        dfc = dfc.sort_values(by='abs_difference_from_average_distance', ascending=False)
-                        for i, row in dfc.head(n_typical_pairs).iterrows():
-                                smi_ref = getattr(row, f'smi_ref_{percentile}')
-                                smi_curr = getattr(row, f'smi_current_{percentile}')
-                                short_taxonomic_chain = '-'.join(row.taxonomic_chain.split('-')[-3:])
-                                print(f"chem dist {getattr(row, f'distance_percentile_{percentile}'):.2f}, {short_taxonomic_chain}, SMILES reference {smi_ref}, SMILES current {smi_curr}")
-                                draw_pairs_of_molecules(smi_curr, smi_ref, 
-                                                        save_typical_molecules_png=f'{data_folder}/typical_pairs_of_molecules/{taxonomic_chain_ref}_td{td}_{i}_{short_taxonomic_chain}.png'
-                                                        )
+        if args.visualize_typical_pairs:
+                for taxonomic_chain_ref in taxonomic_chains_ref:
+                        print(f"Starting with reference taxonomic chain: {taxonomic_chain_ref}")
+                        
+                        tdistance1=2
+                        tdistance2=3
+                        percentile=50
+                        n_typical_pairs=3
+                        
+                        df_chemical_distances_vs_taxonomic_distances = pd.read_csv(f'{data_folder}/chem_vs_tax_dist_csv_files/chem_vs_tax_dist_{taxonomic_chain_ref}.csv')
+                        if len(df_chemical_distances_vs_taxonomic_distances) == 0:
+                                print(f"No data for {taxonomic_chain_ref}")
+                                continue
+                        for td in [tdistance1, tdistance2]:
+                                print(f"Tax dist {td}")
+                                dfc = df_chemical_distances_vs_taxonomic_distances[df_chemical_distances_vs_taxonomic_distances['taxonomic_distance'] == td].copy()
+                                distance_percentile_ave = dfc[f'distance_percentile_{percentile}'].mean()
+                                dfc['abs_difference_from_average_distance'] = (dfc[f'distance_percentile_{percentile}'] - distance_percentile_ave).abs()
+                                dfc = dfc.sort_values(by='abs_difference_from_average_distance', ascending=False)
+                                for i, row in dfc.head(n_typical_pairs).iterrows():
+                                        smi_ref = getattr(row, f'smi_ref_{percentile}')
+                                        smi_curr = getattr(row, f'smi_current_{percentile}')
+                                        short_taxonomic_chain = '-'.join(row.taxonomic_chain.split('-')[-3:])
+                                        print(f"chem dist {getattr(row, f'distance_percentile_{percentile}'):.2f}, {short_taxonomic_chain}, SMILES reference {smi_ref}, SMILES current {smi_curr}")
+                                        draw_pairs_of_molecules(smi_curr, smi_ref, 
+                                                                save_typical_molecules_png=f'{data_folder}/typical_pairs_of_molecules/{taxonomic_chain_ref}_td{td}_{i}_{short_taxonomic_chain}.png'
+                                                                )
