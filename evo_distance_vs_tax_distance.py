50e18d08004dfb8eb1e75b973bca5204c211caea
A S <anton.sinitskiy@gmail.com>
2026-01-17 22:15:56 -0500

    enabled selection of a subset

--- evo_distance_vs_tax_distance.py
+++ evo_distance_vs_tax_distance.py
@@ -9,9 +9,11 @@ def main():
     parser = argparse.ArgumentParser(description="Plot evolutionary distance vs taxonomic distance.")
     parser.add_argument("--csv", default="./data/all_species_distances_upper_triangle.csv", help="Path to all_species_distances_upper_triangle.csv")
     parser.add_argument("--out", default="./data/evo_vs_tax_distance.png", help="Output plot filename")
+    parser.add_argument("--selection", type=str, default="", help="Optional selection condition for filtering the data, e.g., taxonomic_lineage_[12] contain \"Magnoliopsida\"")
     args = parser.parse_args()
     input_csv = Path(args.csv)
     output_png = Path(args.out)
+    selection_condition = args.selection
 
     # input_csv = "./data/all_species_distances_upper_triangle.csv"
     # output_png = "./data/evo_vs_tax_distance.png"
@@ -19,11 +21,13 @@ def main():
     # taxonomic_levels = ['kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species'] # Coconut
 
     df = pd.read_csv(input_csv, usecols=["distance", "tax_lineage_name_1", "tax_lineage_name_2"])
+    df = df[df['tax_lineage_name_1'].str.contains(selection_condition) | df['tax_lineage_name_2'].str.contains(selection_condition)] if selection_condition else df
     df["tax_distance"] = df.apply(lambda r: taxonomic_distance_from_lineages(r["tax_lineage_name_1"], r["tax_lineage_name_2"]), axis=1)
     df = df.dropna(subset=["distance", "tax_distance"])
 
     plt.figure(figsize=(6, 5))
     plt.scatter(df["distance"], df["tax_distance"], s=6)
+    plt.scatter([0], [0], s=6)
     plt.xlabel("Evolutionary distance")
     plt.ylabel("Taxonomic distance")
     plt.yticks(range(int(df["tax_distance"].max()) + 2))
